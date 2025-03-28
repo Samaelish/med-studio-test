137edbb402096b627472b44d39a0fa20bce0bde3
Samaelish <semnd007@gmail.com>
2025-03-28 22:48:35 +0300

    navbar preparation

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ document.querySelectorAll('.program__section-title').forEach(title => {
     lectures.classList.toggle('program__lectures--hidden')
   })
 })
+
+// Responsive and accessible Navigation
+const openButton = document.getElementById('open-navbar-button')
+const navbar = document.querySelector('nav')
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach(
+  link => link.addEventListener('click', () => {
+    closeNavbar()
+  })
+)
+
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e) {
+  const isMobile = e.matches
+  isMobile ?
+    navbar.setAttribute('inert', '') : // Mobile
+    navbar.removeAttribute('inert') // not Mobile
+}
+
+function openNavbar() {
+  navbar.classList.add('show');
+  openButton.setAttribute('aria-expanded', 'true')
+  navbar.removeAttribute('inert')
+}
+function closeNavbar() {
+  navbar.classList.remove('show');
+  openButton.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
+}
+
+// click handler to close the navbar when pressing the Esc key
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && navbar.classList.contains('show')) {
+    closeNavbar()
+  }
+})
+
+updateNavbar(media)
